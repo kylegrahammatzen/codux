53db03cacc443eb3ce0d542f829eca315139bc46
Kyle <kgrahammatzen@gmail.com>
2025-10-17 17:42:08 -0400

    fix(frontend): change breakpoint to xl for chat action text

--- src/components/chat/input-actions.tsx
+++ src/components/chat/input-actions.tsx
@@ -75,7 +75,7 @@ export const ChatInputActions = () => {
             d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
           />
         </svg>
-        <span className="hidden lg:inline">Attach</span>
+        <span className="hidden xl:inline">Attach</span>
       </Button>
       <Button variant="outline" size="sm" className="bg-white">
         <svg
@@ -91,7 +91,7 @@ export const ChatInputActions = () => {
             d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"
           />
         </svg>
-        <span className="hidden lg:inline">Edit</span>
+        <span className="hidden xl:inline">Edit</span>
       </Button>
     </div>
   );
