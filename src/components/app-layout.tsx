04a7e2860861d0ce9de6c7de16b5da0fba6806d7
Kyle <kgrahammatzen@gmail.com>
2025-10-17 17:45:22 -0400

    fix(frontend): hide chat and integrations panels below xl breakpoint

--- src/components/app-layout.tsx
+++ src/components/app-layout.tsx
@@ -13,25 +13,21 @@ export const AppLayout = () => {
 
   return (
     <div className="flex flex-1 overflow-hidden min-h-0">
-      {!isMobile && (
-        <div className={cn(
-          "flex-shrink-0 transition-all ease-in-out duration-300 overflow-hidden",
-          showChat ? "w-[30%] mr-2" : "w-0"
-        )}>
-          <ChatPanel />
-        </div>
-      )}
+      <div className={cn(
+        "hidden xl:block flex-shrink-0 transition-all ease-in-out duration-300 overflow-hidden",
+        showChat ? "w-[30%] mr-2" : "w-0"
+      )}>
+        <ChatPanel />
+      </div>
 
       <PreviewPanel />
 
-      {!isMobile && (
-        <div className={cn(
-          "flex-shrink-0 transition-all ease-in-out duration-300 overflow-hidden",
-          showIntegrations ? "w-[30%] ml-2" : "w-0"
-        )}>
-          <IntegrationsPanel />
-        </div>
-      )}
+      <div className={cn(
+        "hidden xl:block flex-shrink-0 transition-all ease-in-out duration-300 overflow-hidden",
+        showIntegrations ? "w-[30%] ml-2" : "w-0"
+      )}>
+        <IntegrationsPanel />
+      </div>
     </div>
   );
 };
