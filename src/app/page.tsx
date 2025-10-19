baa8e4fa21fe32e8aa61fa16aa47ae3509aef626
Kyle <kgrahammatzen@gmail.com>
2025-10-19 01:37:54 -0400

    feat(frontend): add sandpack and preview providers to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import { EditorLayout } from "@/components/editor-layout";
 import { ProjectProvider } from "@/components/project-context";
+import { SandpackProvider } from "@/components/sandpack-context";
+import { PreviewProvider } from "@/components/preview/preview-context";
 
 export default function Home() {
-  return <ProjectProvider>
-    <EditorLayout />
-  </ProjectProvider>;
+  return (
+    <ProjectProvider>
+      <PreviewProvider>
+        <SandpackProvider>
+          <EditorLayout />
+        </SandpackProvider>
+      </PreviewProvider>
+    </ProjectProvider>
+  );
 }
