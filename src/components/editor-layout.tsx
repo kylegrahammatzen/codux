dcea65dd65fca123655a67fe3e08fb02816705fa
Kyle <kgrahammatzen@gmail.com>
2025-10-21 01:28:54 -0400

    refactor(frontend): extract SandpackLayout to separate component

--- src/components/editor-layout.tsx
+++ src/components/editor-layout.tsx
@@ -1,18 +1,11 @@
 "use client";
 
-import { useProjectContext, DEFAULT_FILES } from "@/components/project-context";
+import { useProjectContext } from "@/components/project-context";
 import { AppHeader } from "@/components/app-header";
-import { ChatPanel } from "@/components/chat/panel";
-import { PreviewPanel } from "@/components/preview/panel";
-import { PanelWrapper } from "@/components/panel-wrapper";
-import { SandpackProvider } from "@codesandbox/sandpack-react";
+import { SandpackLayout } from "@/components/sandpack/layout";
 import { cn } from "@/lib/utils";
 
-type LayoutWrapperProps = {
-  children: React.ReactNode;
-};
-
-const LayoutWrapper = (props: LayoutWrapperProps) => {
+export const EditorLayout = () => {
   const { fullscreen } = useProjectContext();
 
   return (
@@ -22,34 +15,8 @@ const LayoutWrapper = (props: LayoutWrapperProps) => {
         fullscreen ? "gap-0 p-0" : "gap-2 p-2"
       )}
     >
-      {props.children}
-    </div>
-  );
-};
-
-export const EditorLayout = () => {
-  return (
-    <LayoutWrapper>
       <AppHeader />
-
-      <SandpackProvider
-        style={{ display: 'contents' }}
-        files={DEFAULT_FILES}
-        customSetup={{
-          dependencies: {
-            react: "^18.3.1",
-            "react-dom": "^18.3.1",
-          },
-        }}
-      >
-        <div className="flex flex-1 overflow-hidden min-h-0">
-          <PanelWrapper>
-            <ChatPanel />
-          </PanelWrapper>
-
-          <PreviewPanel />
-        </div>
-      </SandpackProvider>
-    </LayoutWrapper>
+      <SandpackLayout />
+    </div>
   );
 };
---
+++ src/components/sandpack/layout.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { DEFAULT_FILES } from "@/components/project-context";
+import { ChatPanel } from "@/components/chat/panel";
+import { PreviewPanel } from "@/components/preview/panel";
+import { PanelWrapper } from "@/components/panel-wrapper";
+import { SandpackProvider } from "@codesandbox/sandpack-react";
+
+export const SandpackLayout = () => {
+  return (
+    <SandpackProvider
+      style={{ display: 'contents' }}
+      files={DEFAULT_FILES}
+      customSetup={{
+        dependencies: {
+          react: "^18.3.1",
+          "react-dom": "^18.3.1",
+        },
+      }}
+    >
+      <div className="flex flex-1 overflow-hidden min-h-0">
+        <PanelWrapper>
+          <ChatPanel />
+        </PanelWrapper>
+
+        <PreviewPanel />
+      </div>
+    </SandpackProvider>
+  );
+};
