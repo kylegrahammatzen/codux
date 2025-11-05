0818856c561b37567f937f029c846a7beece69d5
Kyle <kgrahammatzen@gmail.com>
2025-11-05 07:40:43 -0500

    refactor(frontend): restructure to single shared TamboProvider with contextKey

--- src/app/(dashboard)/(tambo)/client-layout.tsx
+++ src/app/(dashboard)/(tambo)/client-layout.tsx
@@ -3,6 +3,7 @@
 import { TamboProvider } from "@tambo-ai/react";
 import { env } from "@/env";
 import { FileUploadProvider } from "@/contexts/file-upload-context";
+import { useContextKey } from "@/hooks/use-context-key";
 
 type ClientLayoutProps = {
   children: React.ReactNode;
@@ -10,9 +11,15 @@ type ClientLayoutProps = {
 };
 
 export const ClientLayout = (props: ClientLayoutProps) => {
+  const contextKey = useContextKey();
+
   return (
     <FileUploadProvider>
-      <TamboProvider apiKey={env.NEXT_PUBLIC_TAMBO_API_KEY} userToken={props.userToken}>
+      <TamboProvider
+        apiKey={env.NEXT_PUBLIC_TAMBO_API_KEY}
+        userToken={props.userToken}
+        contextKey={contextKey || undefined}
+      >
         {props.children}
       </TamboProvider>
     </FileUploadProvider>
